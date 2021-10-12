43d405639ad6a8e3094dd6cae2023d920995a4f1
Nurul jaelani <nuruljaelani96@gmail.com>
2021-10-12 21:12:27 +0700

    navigation

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,16 @@
 <template>
-  <div class="bg-white shadow rounded border w-1/2 mx-auto my-10">
-    <p>Hello World</p>
+  <div class="bg-white shadow border w-full py-2 md:py-3">
+    <div class="px-10 md:px-20 lg:px-28 flex items-center justify-between">
+      <p class="text-lg font-semibold">
+        Blog
+      </p>
+      <div class="flex space-x-4 md:space-x-8 lg:space-x-12">
+        <p>Home</p>
+        <p>Article</p>
+        <p>Tutorial</p>
+        <p>About</p>
+      </div>
+    </div>
   </div>
 </template>
 
