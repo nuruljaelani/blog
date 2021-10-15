4023bab3dbca2b775f11462a36f543c72ce4f727
Nurul Jaelani <nuruljaelani96@gmail.com>
2021-10-15 11:36:09 +0700

    add illustration

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,23 @@
 <template>
-  <div class="bg-white shadow border w-full py-2 md:py-3">
-    <div class="px-10 md:px-20 lg:px-28 flex items-center justify-between">
-      <p class="text-lg font-semibold">
-        Blog
-      </p>
-      <div class="flex space-x-4 md:space-x-8 lg:space-x-12">
-        <p>Home</p>
-        <p>Article</p>
-        <p>Tutorial</p>
-        <p>About</p>
+  <div>
+    <nav>
+      <div class="flex items-center justify-between mx-auto max-w-7xl py-2 lg:py-3">
+        <p class="text-lg lg:text-xl font-bold">
+          Go Blog
+        </p>
+        <div class="flex space-x-4 md:space-x-8 lg:space-x-12 font-medium">
+          <p>Home</p>
+          <p>Article</p>
+          <p>Tutorial</p>
+          <p>About</p>
+        </div>
       </div>
-    </div>
+    </nav>
+    <main>
+      <div class="flex w-1/2">
+        <img src="~/assets/Scenes05.svg" alt="illustration">
+      </div>
+    </main>
   </div>
 </template>
 
